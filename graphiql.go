eeb4ca7cdfacb01e1c606cfe9690975600943c2a
Bervianto Leo Pratama <bervianto.leo@gmail.com>
2022-04-18 13:08:18 +0700

    Change react  path

--- graphiql.go
+++ graphiql.go
@@ -100,8 +100,8 @@ add "&raw" to the end of the URL within a browser.
   <link href="//cdn.jsdelivr.net/npm/graphiql@{{ .GraphiqlVersion }}/graphiql.css" rel="stylesheet" />
   <script src="//cdn.jsdelivr.net/es6-promise/4.0.5/es6-promise.auto.min.js"></script>
   <script src="//cdn.jsdelivr.net/fetch/0.9.0/fetch.min.js"></script>
-  <script src="//cdn.jsdelivr.net/react/16.14.0/react.min.js"></script>
-  <script src="//cdn.jsdelivr.net/react/16.14.0/react-dom.min.js"></script>
+  <script src="//cdn.jsdelivr.net/npm/react@16.14.0/index.min.js"></script>
+  <script src="//cdn.jsdelivr.net/npm/react-dom@16.14.0/index.min.js"></script>
   <script src="//cdn.jsdelivr.net/npm/graphiql@{{ .GraphiqlVersion }}/graphiql.min.js"></script>
 </head>
 <body>
